5fd1fcbd8af6991c968ea7739a7c83a7277dd658
Narciso Gomes <narcisoalmeida25@gmail.com>
2025-12-23 19:38:30 -0300

    feat: cria toolbar com thema personalizado

--- src/app/core/layout/components/header/header.ts
+++ src/app/core/layout/components/header/header.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatToolbar],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
-
-}
+export class Header {}
