3cd69dd8184a9cd2932c26b117837f44f1464d94
Narciso Gomes <narcisoalmeida25@gmail.com>
2025-12-30 23:27:54 -0300

    feat: adiciona estilizacao de cores nos valores do balance card

--- src/app/features/home/components/balance-card/balance-card.ts
+++ src/app/features/home/components/balance-card/balance-card.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
@@ -11,5 +11,16 @@ export class BalanceCard {
   type = input.required<'income' | 'outcome' | 'balance'>();
   label = input.required<string>();
   value = input.required<number>();
-  
+
+  cssClass = computed(() => {
+    if (this.type() === 'income') {
+      return 'income';
+    }
+    
+    if (this.type() === 'outcome') {
+      return 'outcome';
+    }
+
+    return this.value() > 0 ? 'income' : 'outcome';
+  });
 }
