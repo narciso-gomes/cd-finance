c905717bbcd34bdb93dd18c98d6828eb5748d1ba
Narciso Gomes <narcisoalmeida25@gmail.com>
2025-12-30 23:37:23 -0300

    feat: melhora tipagem dos inputs do balance-card

--- src/app/features/home/components/balance-card/balance-card.ts
+++ src/app/features/home/components/balance-card/balance-card.ts
@@ -1,6 +1,8 @@
 import { Component, computed, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
+type CardType = 'income' | 'outcome' | 'balance';
+type ValueCssClass = 'income' | 'outcome';
 @Component({
   selector: 'app-balance-card',
   imports: [MatCardModule],
@@ -8,15 +10,15 @@ import { MatCardModule } from '@angular/material/card';
   styleUrl: './balance-card.scss',
 })
 export class BalanceCard {
-  type = input.required<'income' | 'outcome' | 'balance'>();
+  type = input.required<CardType>();
   label = input.required<string>();
   value = input.required<number>();
 
-  cssClass = computed(() => {
+  cssClass = computed<ValueCssClass>(() => {
     if (this.type() === 'income') {
       return 'income';
     }
-    
+
     if (this.type() === 'outcome') {
       return 'outcome';
     }
